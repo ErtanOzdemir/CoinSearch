95a3158b64a6739a874a0dec7a3c23f1e20beec2
Ertan Özdemir <ertan_kubi@hotmail.com>
2021-03-20 16:17:57 +0300

    Property names were changed in Vue instance (app.js) because of the clean code reasons. Also, these new property names implemented to index.html file.

--- app.js
+++ app.js
@@ -2,35 +2,37 @@
      el: '#app',
      data() {
          return {
-             info: null,
-             obj: null,
-             logo: null,
-             name: null,
-             searchName: null,
-             id: null,
+             descriptionOfCoin: null,
+             logoOfCoin: null,
+             nameOfCoin: null,
+             enteredAcronymNameOfCoin: null,
+             acronymOfCoin: null,
 
-             facebookAdress: null,
-             redditAdress: null,
-             twitterAdress: null,
-             mediumAdress: null,
+             adressAt: {
+                 facebook: null,
+                 reddit: null,
+                 twitter: null,
+                 medium: null,
+             }
 
 
          }
      },
      methods: {
          searchCoin() {
-             fetch("https://api.nomics.com/v1/currencies?key=74420a7d3bc34353bcd21da30533efe8&ids=" + this.searchName.toUpperCase())
+             fetch("https://api.nomics.com/v1/currencies?key=74420a7d3bc34353bcd21da30533efe8&ids=" + this.enteredAcronymNameOfCoin.toUpperCase())
                  .then(response => response.json())
                  .then(data => {
                      console.log(data);
-                     this.info = data[0].description;
-                     this.logo = data[0].logo_url;
-                     this.name = data[0].name;
-                     this.id = data[0].id;
-                     this.redditAdress = data[0].reddit_url;
-                     this.facebookAdress = data[0].facebook_url;
-                     this.mediumAdress = data[0].medium_url;
-                     this.twitterAdress = data[0].twitter_url;
+                     this.descriptionOfCoin = data[0].description;
+                     this.logoOfCoin = data[0].logo_url;
+                     this.nameOfCoin = data[0].name;
+                     this.acronymOfCoin = data[0].id;
+
+                     this.adressAt.reddit = data[0].reddit_url;
+                     this.adressAt.facebook = data[0].facebook_url;
+                     this.adressAt.medium = data[0].medium_url;
+                     this.adressAt.twitter = data[0].twitter_url;
 
                  }).catch(() => alert("We couldn't find the money you were looking for."))
          },
